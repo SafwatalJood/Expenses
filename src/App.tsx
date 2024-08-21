b9a5038ab684c5f724bec333f00c7751e527d443
SafwatalJood <almalkyf255@gmail.com>
2024-08-21 20:10:05 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
 import Profile from "./components/Profile";
 import Home from "./components/Home";
